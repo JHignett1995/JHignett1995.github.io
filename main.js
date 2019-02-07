a128ab7cdaf543bafc4ef6263356a580538f3639
JHignett1995 <jordan.hignett1@googlemail.com>
2019-02-07 10:56:48 +0000

    orghaeuo

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 <script>
-
+    function openPageTwo(){
+        open("https://jhignett1995.github.io/page2/");
+    };
+    function openPageThree(){
+            open("https://jhignett1995.github.io/page3", , , );
+    }
 </script>
